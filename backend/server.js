9a7dbd9a7b863f724d1a6bee14e8956e474ac4e8
Francis Ge <francisge527@gmail.com>
2026-03-03 15:16:02 +0800

    add routes for auth and add cookiParser middleware

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import emailRoutes from "./routes/email.routes.js";
+import authRoutes from "./routes/auth.routes.js";
 
 dotenv.config();
 
@@ -9,13 +11,15 @@ const PORT = process.env.PORT || 3000;
 
 //middlewares
 app.use(express.json());
+app.use(cookieParser());
 
 //routes
+app.use("/api/auth", authRoutes);
 app.use("/api", emailRoutes);
 
 //default, for checking only
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Bulk-mailer API is running");
 });
 
 app.listen(PORT, () => {
