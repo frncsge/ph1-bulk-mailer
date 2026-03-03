9f75ca67e418380c07a26ed7362f02b6a75d29b3
Francis Ge <francisge527@gmail.com>
2026-03-03 15:13:24 +0800

    update process.env.PASSWORD to .APP_PASSWORD

--- backend/config/email.config.js
+++ backend/config/email.config.js
@@ -7,7 +7,7 @@ const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     user: process.env.EMAIL,
-    pass: process.env.PASSWORD,
+    pass: process.env.APP_PASSWORD,
   },
 });
 
