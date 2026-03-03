ba7fc3de84dfa91c362219aa8e538b167970e091
Francis Ge <francisge527@gmail.com>
2026-03-03 15:14:34 +0800

    add routes for user authentication

--- backend/routes/email.routes.js
+++ backend/routes/email.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import upload from "../config/multer.config.js";
 import { sendEmail } from "../controllers/email.controller.js";
+import { authenticateUser } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
-router.post("/email/send", upload.array("attachments"), sendEmail);
+router.post("/email/send", authenticateUser, upload.array("attachments"), sendEmail);
 
 export default router;
