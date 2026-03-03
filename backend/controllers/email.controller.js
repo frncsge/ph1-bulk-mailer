9ca14b286b1254c76e473a0523b65319bc719b67
Francis Ge <francisge527@gmail.com>
2026-03-03 19:47:20 +0800

    rename body to template and add comments

--- backend/controllers/email.controller.js
+++ backend/controllers/email.controller.js
@@ -1,17 +1,19 @@
 import { sendBulkEmails } from "../utils/email.utils.js";
-import { createEmailAttachments } from "../utils/email.utils.js";
+import {
+  createEmailAttachments
+} from "../utils/email.utils.js";
 
 export const sendEmail = async (req, res) => {
-  const { sender, recipients, subject, body } = req.body;
+  const { sender, recipients, subject, template } = req.body;
   const files = req.files;
   let parsedRecipients;
 
   // check for missing input fields
-  if (!sender || !recipients || !subject || !body) {
+  if (!sender || !recipients || !subject || !template) {
     return res.status(400).json({ message: "Incomplete input" });
   }
 
-  // parse recipients field
+  // parse recipients field back into an array
   try {
     parsedRecipients =
       typeof recipients === "string" ? JSON.parse(recipients) : recipients;
@@ -22,28 +24,28 @@ export const sendEmail = async (req, res) => {
     });
   }
 
-  // check each recipients for their name and email (must have both)
+  // find recipients that does not have an email
   const invalidRecipient = parsedRecipients.find(
-    (recipient) => !recipient.name || !recipient.email,
+    (recipient) => !recipient.email,
   );
   if (invalidRecipient)
     return res
       .status(400)
-      .json({ message: "Each recipient must have their name and email" });
+      .json({ message: "Each recipient must have their email" });
 
-  //set up arguments for sending the email
+  // convert uploaded files into a format compatible with nodemailer attachments
   const emailAttachments = files && createEmailAttachments(files);
-  const email = {
-    sender,
-    recipients: parsedRecipients,
-    subject,
-    body,
-    attachments: emailAttachments || [],
-  };
 
   // send email
   try {
-    await sendBulkEmails(email);
+    await sendBulkEmails({
+      sender,
+      recipients: parsedRecipients,
+      subject,
+      template,
+      attachments: emailAttachments || [],
+    });
+
     res.status(200).json({ message: "Emails sent" });
   } catch (error) {
     console.error("Error sending email", error);
