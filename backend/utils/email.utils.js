6813d93bc9c0180e82388ce43af53f07a5bad024
Francis Ge <francisge527@gmail.com>
2026-03-04 23:46:07 +0800

    track successful and failed email sending and return it to the caller

--- backend/utils/email.utils.js
+++ backend/utils/email.utils.js
@@ -7,8 +7,10 @@ export const sendBulkEmails = async ({
   template,
   attachments,
 }) => {
-  for (const recipient of recipients) { 
+  const successful = [];
+  const failed = [];
 
+  for (const recipient of recipients) {
     const emailMessage = {
       from: `${sender} <${process.env.EMAIL}>`,
       to: recipient.email,
@@ -17,12 +19,26 @@ export const sendBulkEmails = async ({
       attachments,
     };
 
-    await transporter.sendMail(emailMessage);
-    console.log(`Email sent to ${recipient.email}`);
+    try {
+      await transporter.sendMail(emailMessage);
+
+      // push successful emails
+      successful.push(recipient.email);
+      console.log(`Email sent to ${recipient.email}`);
+    } catch (error) {
+      // push failed emails
+      failed.push(recipient.email);
+      console.error(
+        `Failed to send email to ${recipient.email}:`,
+        error.message,
+      );
+    }
   }
+
+  return { successful, failed };
 };
 
-// replace placeholders in the template with 
+// replace placeholders in the template with
 // the recipient data to make the email message
 const interpolateTemplate = (template, recipient) => {
   return template.replace(/{{(.*?)}}/g, (_, captured) => {
